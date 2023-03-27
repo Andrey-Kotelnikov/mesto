5f5825b3cc7ffccfce637676da2fb7939b86b055
Andrey Kotelnikov <dobreutore@yandex.ru>
2023-03-28 00:11:37 +0300

    feat: добавить popup с рабочей логикой

---
+++ script.js
@@ -0,0 +1,43 @@
+let popupElement = document.querySelector('.popup');
+let popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+let popupOpenButtonElement = document.querySelector('.profile__edit-button');
+
+function openPopup() {
+  popupElement.classList.add('popup_is-opened');
+}
+
+function closePopup() {
+  popupElement.classList.remove('popup_is-opened');
+}
+
+function closePopupByClickOnOverlay (event) {
+  console.log(event.target, event.currentTarget);
+  if (event.target === event.currentTarget) {
+    closePopup();
+  }
+}
+
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
+
+
+let nameInput = popupElement.querySelector('.popup__name');
+let jobInput = popupElement.querySelector('.popup__status');
+
+let profileNameElement = document.querySelector('.profile__title');
+let profileJobElement = document.querySelector('.profile__subtitle');
+
+nameInput.value = profileNameElement.textContent;
+jobInput.value = profileJobElement.textContent;
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+
+    profileNameElement.textContent = nameInput.value;
+    profileJobElement.textContent = jobInput.value;
+
+    closePopup();
+}
+
+popupElement.addEventListener('submit', handleFormSubmit);
